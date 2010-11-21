a62b637c0744605c35e122ad6b1a917610d2fcaa
Demis Bellot <demis.bellot@gmail.com>
2010-11-21 21:40:45 +0000

    first commit of new git src repo

---
+++ build/build.bat
@@ -0,0 +1,16 @@
+SET DEPLOY_PATH=C:\src\ServiceStack\release\latest\ServiceStack.Redis
+
+REM SET BUILD=Debug
+SET BUILD=Release
+
+SET PROJ_LIBS=
+SET PROJ_LIBS=%PROJ_LIBS% ..\lib\ServiceStack.Interfaces.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\lib\ServiceStack.Client.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\lib\ServiceStack.Common.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\lib\ServiceStack.Text.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\lib\ServiceStack.Messaging.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.dll
+
+ILMerge.exe /ndebug /t:library /out:ServiceStack.Redis.dll %PROJ_LIBS%
+
+COPY *.dll %DEPLOY_PATH%
