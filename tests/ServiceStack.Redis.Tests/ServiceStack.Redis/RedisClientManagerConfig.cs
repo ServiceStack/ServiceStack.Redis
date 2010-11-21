e59aa24490e6a0a54c9803bb93e1d4a5a8a8e73a
Demis Bellot <demis.bellot@gmail.com>
2010-11-21 21:47:26 +0000

    ignore bin/ obj/ dirs and check in tests/

---
+++ tests/ServiceStack.Redis.Tests/ServiceStack.Redis/RedisClientManagerConfig.cs
@@ -0,0 +1,27 @@
+//
+// https://github.com/mythz/ServiceStack.Redis
+// ServiceStack.Redis: ECMA CLI Binding to the Redis key-value storage system
+//
+// Authors:
+//   Demis Bellot (demis.bellot@gmail.com)
+//
+// Copyright 2010 Liquidbit Ltd.
+//
+// Licensed under the same terms of Redis and ServiceStack: new BSD license.
+//
+
+namespace ServiceStack.Redis
+{
+	public class RedisClientManagerConfig
+	{
+		public RedisClientManagerConfig()
+		{
+			AutoStart = true; //Simplifies the most common use-case - registering in an IOC
+		}
+
+		public int? DefaultDb { get; set; }
+		public int MaxReadPoolSize { get; set; }
+		public int MaxWritePoolSize { get; set; }
+		public bool AutoStart { get; set; }
+	}
+}
---
+++ tests/ServiceStack.Redis.Tests/ServiceStack.Redis/RedisResponseException.cs
@@ -0,0 +1,30 @@
+//
+// https://github.com/mythz/ServiceStack.Redis
+// ServiceStack.Redis: ECMA CLI Binding to the Redis key-value storage system
+//
+// Authors:
+//   Demis Bellot (demis.bellot@gmail.com)
+//
+// Copyright 2010 Liquidbit Ltd.
+//
+// Licensed under the same terms of Redis and ServiceStack: new BSD license.
+//
+
+namespace ServiceStack.Redis
+{
+	public class RedisResponseException
+		: RedisException
+	{
+		public RedisResponseException(string message)
+			: base(message)
+		{
+		}
+
+		public RedisResponseException(string message, string code) : base(message)
+		{
+			Code = code;
+		}
+
+		public string Code { get; private set; }
+	}
+}
