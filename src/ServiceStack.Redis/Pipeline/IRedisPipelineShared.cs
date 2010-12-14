2b88cb2be91588a39a73a4b8fa83d860e2d27573
Demis Bellot <demis.bellot@gmail.com>
2010-12-14 20:29:01 +0000

    Changed desc

--- src/ServiceStack.Redis/Pipeline/IRedisPipelineShared.cs
+++ src/ServiceStack.Redis/Pipeline/IRedisPipelineShared.cs
@@ -3,7 +3,7 @@
 namespace ServiceStack.Redis.Pipeline
 {
 	/// <summary>
-	/// Base pipeline interface, shared by typed and non-typed pipelines
+	/// Pipeline interface shared by typed and non-typed pipelines
 	/// </summary>
 	public interface IRedisPipelineShared : IDisposable, IRedisQueueCompletableOperation
 	{
