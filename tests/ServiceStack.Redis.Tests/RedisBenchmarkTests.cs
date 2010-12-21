9985e2158454f44407e1df681a10c40d4886a88f
Aaron Boxer <Aaron.Boxer@uhn.on.ca>
2010-12-20 21:34:58 -0500

    added benchmark test comparing SORT on a set of keys
    to SMEMBERS followed by MGET

--- tests/ServiceStack.Redis.Tests/RedisBenchmarkTests.cs
+++ tests/ServiceStack.Redis.Tests/RedisBenchmarkTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using NUnit.Framework;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+
 
 namespace ServiceStack.Redis.Tests
 {
@@ -11,19 +14,97 @@ namespace ServiceStack.Redis.Tests
         const string Value = "Value";
 
         [Test]
-        public void Performance()
+        public void Measure_pipeline_speedup()
         {
-            var sw = Stopwatch.StartNew();
+            string key = "key";
             int total = 500;
             var temp = new byte[1];
             for (int i = 0; i < total; ++i)
             {
-                ((RedisNativeClient)Redis).Set("key", temp);
+                Redis.Del(key + i.ToString());
+            }
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < total; ++i)
+            {
+                ((RedisNativeClient)Redis).Set(key + i.ToString(), temp);
             }
             sw.Stop();
             Console.WriteLine(String.Format("Time for {0} Set(key,value) operations: {1} ms", total, sw.ElapsedMilliseconds));
+
+            for (int i = 0; i < total; ++i)
+            {
+                Redis.Del(key + i.ToString());
+            }
+            sw = Stopwatch.StartNew();
+            using (var pipeline = Redis.CreatePipeline())
+            {
+                for (int i = 0; i < total; ++i)
+                         pipeline.QueueCommand(r => ((RedisNativeClient)Redis).Set(key + i.ToString(),temp));
+                pipeline.Flush();
+
+            }
+            sw.Stop();
+            Console.WriteLine(String.Format("Time for pipelining {0} Set(key,value) operations: {1} ms", total, sw.ElapsedMilliseconds));
+
+
         }
 
+        private string[] stringsFromBytes(byte[][] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return new string[1];
+
+            }
+            var rc  = new string[input.Length];
+            for (int i = 0; i < input.Length; ++i )
+            {
+                rc[i] = input[i].FromUtf8Bytes();
+            }
+            return rc;
+        }
+        [Test]
+        public void Compare_sort_nosort_to_smembers_mget()
+        {
+            string setKey = "setKey";
+            int total = 255;
+            int count = 20;
+            var temp = new byte[5];
+            temp[0] = 0;
+            temp[1] = 1;
+            temp[2] = 2;
+            temp[3] = 3;
+            temp[4] = 0;
+
+            //initialize set and individual keys
+            Redis.Del(setKey);
+            for (var i = 0; i < total; ++i)
+            {
+                string key = setKey + i;
+                Redis.SAdd(setKey, key.ToUtf8Bytes());
+                Redis.Set(key, temp);
+            }
+
+            var sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < count; ++i)
+            {
+                var keys = stringsFromBytes(Redis.SMembers(setKey));
+                stringsFromBytes(Redis.MGet(keys));
+            }
+
+            sw.Stop();
+            Console.WriteLine(String.Format("Time to call {0} SMembers and MGet operations: {1} ms", count, sw.ElapsedMilliseconds));
+            var opt = new SortOptions() {SortPattern = "nosort", GetPattern = "*"};
+
+            sw = Stopwatch.StartNew();
+            for (int i = 0; i < count; ++i)
+               stringsFromBytes(Redis.Sort(setKey, opt));
+            sw.Stop();
+            Console.WriteLine(String.Format("Time to call {0} sort operations: {1} ms", count, sw.ElapsedMilliseconds));
+
+
+        }
     }
 
 }
