7e7db060e703080a512d8bc857af4dcda90a37b4
mythz <demis.bellot@gmail.com>
2011-10-26 19:01:19 -0400

    updated dlls

---
+++ src/ServiceStack.Redis/Messaging/BackgroundThreadMqHost.cs
@@ -0,0 +1,308 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using ServiceStack.Logging;
+using ServiceStack.Messaging;
+using ServiceStack.Text;
+
+namespace ServiceStack.Redis.Messaging
+{
+    public class BackgroundThreadMqHost : IMessageService
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(BackgroundThreadMqHost));
+        public const int DefaultRetryCount = 2; //Will be a total of 3 attempts
+
+        readonly Random rand = new Random(Environment.TickCount);
+        private void SleepBackOffMultiplier(int continuousErrorsCount)
+        {
+            if (continuousErrorsCount == 0) return;
+            const int MaxSleepMs = 60 * 1000;
+
+            //exponential/random retry back-off.
+            var nextTry = Math.Min(
+                rand.Next((int)Math.Pow(continuousErrorsCount, 3), (int)Math.Pow(continuousErrorsCount + 1, 3) + 1),
+                MaxSleepMs);
+
+            Log.Debug("Sleeping for {0}ms after {1} continuous errors".Fmt(nextTry, continuousErrorsCount));
+
+            Thread.Sleep(nextTry);
+        }
+
+        //Control Commands
+        public const string StopCommand = "STOP";
+
+        //States
+        public const int Disposed = -1;
+        public const int Stopped = 0;
+        public const int Stopping = 1;
+        public const int Starting = 2;
+        public const int Started = 3;
+
+        //Stats
+        private long timesStarted = 0;
+        private long noOfErrors = 0;
+        private int noOfContinuousErrors = 0;
+        private string lastExMsg = null;
+        private int status;
+
+        private long bgThreadCount = 0;
+        public long BgThreadCount
+        {
+            get { return Interlocked.CompareExchange(ref bgThreadCount, 0, 0); }
+        }
+
+        public int RetryCount { get; protected set; }
+        public TimeSpan? RequestTimeOut { get; protected set; }
+
+        private readonly IRedisClientsManager clientsManager; //Thread safe redis client/conn factory
+
+        public IMessageQueueClient CreateMessageQueueClient()
+        {
+            return new RedisMessageQueueClient(this.clientsManager, null);
+        }
+
+        public BackgroundThreadMqHost(IRedisClientsManager clientsManager,
+            int retryCount, TimeSpan? requestTimeOut)
+        {
+            this.clientsManager = clientsManager;
+            this.RetryCount = retryCount;
+            this.RequestTimeOut = requestTimeOut;
+        }
+
+        private readonly Dictionary<Type, IMessageHandlerFactory> handlerMap
+            = new Dictionary<Type, IMessageHandlerFactory>();
+
+        private IMessageHandler[] messageHandlers;
+        private string[] inQueueNames;
+
+        public void RegisterHandler<T>(Func<IMessage<T>, object> processMessageFn)
+        {
+            RegisterHandler(processMessageFn, null);
+        }
+
+        public void RegisterHandler<T>(Func<IMessage<T>, object> processMessageFn, Action<Exception> processExceptionEx)
+        {
+            if (handlerMap.ContainsKey(typeof(T)))
+            {
+                throw new ArgumentException("Message handler has already been registered for type: " + typeof(T).Name);
+            }
+
+            handlerMap[typeof(T)] = CreateMessageHandlerFactory(processMessageFn, processExceptionEx);
+        }
+
+        protected IMessageHandlerFactory CreateMessageHandlerFactory<T>(Func<IMessage<T>, object> processMessageFn, Action<Exception> processExceptionEx)
+        {
+            return new MessageHandlerFactory<T>(this, processMessageFn, processExceptionEx)
+            {
+                RetryCount = RetryCount
+            };
+        }
+
+        private void RunLoop()
+        {
+            if (Interlocked.CompareExchange(ref status, Started, Starting) != Starting) return;
+            Interlocked.Increment(ref timesStarted);
+
+            try
+            {
+                while (true)
+                {
+                    //Pass in a new MQ Client that may be used by message handlers
+                    using (var mqClient = CreateMessageQueueClient())
+                    {
+                        foreach (var handler in messageHandlers)
+                        {
+                            if (Interlocked.CompareExchange(ref status, Stopped, Stopping) == Stopping)
+                            {
+                                Log.Debug("MQ Host is stopping, exiting RunLoop()...");
+                                return;
+                            }
+                            if (Interlocked.CompareExchange(ref status, 0, 0) != Started)
+                            {
+                                Log.Error("MQ Host is in an invalid state '{0}', exiting RunLoop()...".Fmt(GetStatus()));
+                                return;
+                            }
+                            handler.Process(mqClient);
+                        }
+
+                        //Record that we had a good run...
+                        Interlocked.CompareExchange(ref noOfContinuousErrors, 0, noOfContinuousErrors);
+
+                        var cmd = mqClient.WaitForNotifyOnAny(QueueNames.Topic);
+                        if (cmd == StopCommand)
+                        {
+                            Log.Debug("Stop Command Issued");
+                            if (Interlocked.CompareExchange(ref status, Stopped, Started) != Started)
+                                Interlocked.CompareExchange(ref status, Stopped, Stopping);
+
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lastExMsg = ex.Message;
+                Interlocked.Increment(ref noOfErrors);
+                Interlocked.Increment(ref noOfContinuousErrors);
+
+                if (Interlocked.CompareExchange(ref status, Stopped, Started) != Started)
+                    Interlocked.CompareExchange(ref status, Stopped, Stopping);
+
+                Log.Error("Exception in Background Thread: " + ex.Message, ex);
+            }
+        }
+
+        private Thread bgThread;
+
+        public virtual void Start()
+        {
+            if (Interlocked.CompareExchange(ref status, 0, 0) == Started) return;
+            if (Interlocked.CompareExchange(ref status, 0, 0) == Disposed)
+                throw new ObjectDisposedException("MQ Host has been disposed");
+
+            if (Interlocked.CompareExchange(ref status, Starting, Stopped) == Stopped) //Should only be 1 thread past this point
+            {
+                Init();
+
+                if (this.messageHandlers == null || this.messageHandlers.Length == 0)
+                {
+                    Log.Warn("Cannot start a MQ Host with no Message Handlers registered, ignoring.");
+                    Interlocked.CompareExchange(ref status, Stopped, Starting);
+                    return;
+                }
+
+                SleepBackOffMultiplier(Interlocked.CompareExchange(ref noOfContinuousErrors, 0, 0));
+
+                KillBgThreadIfExists();
+
+                bgThread = new Thread(RunLoop)
+                {
+                    IsBackground = true,
+                    Name = "Redis MQ Host " + Interlocked.Increment(ref bgThreadCount)
+                };
+                bgThread.Start();
+                Log.Debug("Started Background Thread: " + bgThread.Name);
+            }
+        }
+
+        private void KillBgThreadIfExists()
+        {
+            if (bgThread != null && bgThread.IsAlive)
+            {
+                //Ideally we shouldn't get here, but lets try our hardest to clean it up
+                Log.Warn("Interrupting previous Background Thread: " + bgThread.Name);
+                bgThread.Interrupt();
+                if (!bgThread.Join(TimeSpan.FromSeconds(3)))
+                {
+                    Log.Warn(bgThread.Name + " just wont die, so we're now aborting it...");
+                    bgThread.Abort();
+                }
+                bgThread = null;
+            }
+        }
+
+        private void Init()
+        {
+            if (this.messageHandlers == null)
+            {
+                this.messageHandlers = this.handlerMap.Values.ToList()
+                    .ConvertAll(x => x.CreateMessageHandler()).ToArray();
+            }
+            if (inQueueNames == null)
+            {
+                inQueueNames = this.handlerMap.Keys.ToList()
+                    .ConvertAll(x => new QueueNames(x).In).ToArray();
+            }
+        }
+
+        public string GetStatus()
+        {
+            switch (Interlocked.CompareExchange(ref status, 0, 0))
+            {
+                case Disposed:
+                    return "Disposed";
+                case Stopped:
+                    return "Stopped";
+                case Stopping:
+                    return "Stopping";
+                case Starting:
+                    return "Starting";
+                case Started:
+                    return "Started";
+            }
+            return null;
+        }
+
+        public IMessageHandlerStats GetStats()
+        {
+            lock (messageHandlers)
+            {
+                var total = new MessageHandlerStats("All Handlers");
+                messageHandlers.ToList().ForEach(x => total.Add(x.GetStats()));
+                return total;
+            }
+        }
+
+        public string GetStatsDescription()
+        {
+            lock (messageHandlers)
+            {
+                var sb = new StringBuilder("#MQ HOST STATS:\n");
+                sb.AppendLine("===============");
+                sb.AppendLine("Current Status: " + GetStatus());
+                sb.AppendLine("Listening On: " + string.Join(", ", inQueueNames));
+                sb.AppendLine("Times Started: " + Interlocked.CompareExchange(ref timesStarted, 0, 0));
+                sb.AppendLine("Num of Errors: " + Interlocked.CompareExchange(ref noOfErrors, 0, 0));
+                sb.AppendLine("Num of Continuous Errors: " + Interlocked.CompareExchange(ref noOfContinuousErrors, 0, 0));
+                sb.AppendLine("Last ErrorMsg: " + lastExMsg);
+                sb.AppendLine("===============");
+                foreach (var messageHandler in messageHandlers)
+                {
+                    sb.AppendLine(messageHandler.GetStats().ToString());
+                    sb.AppendLine("---------------\n");
+                }
+                return sb.ToString();
+            }
+        }
+
+        public virtual void Stop()
+        {
+            if (Interlocked.CompareExchange(ref status, 0, 0) == Disposed)
+                throw new ObjectDisposedException("MQ Host has been disposed");
+
+            if (Interlocked.CompareExchange(ref status, Stopping, Started) == Started)
+            {
+                Log.Debug("Stopping MQ Host...");
+
+                //Unblock current bgthread by issuing StopCommand
+                try
+                {
+                    using (var redis = clientsManager.GetClient())
+                    {
+                        redis.PublishMessage(QueueNames.Topic, StopCommand);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Warn("Could not send STOP message to bg thread: " + ex.Message);
+                }
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            Stop();
+
+            if (Interlocked.CompareExchange(ref status, Disposed, Stopped) != Stopped)
+                Interlocked.CompareExchange(ref status, Disposed, Stopping);
+
+            Thread.Sleep(100); //give it a small chance to die gracefully
+            KillBgThreadIfExists();
+        }
+
+    }
+
+}
--- tests/ServiceStack.Redis.Tests/BackgroundThreadMessageServiceTests.cs
+++ tests/ServiceStack.Redis.Tests/BackgroundThreadMessageServiceTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading;
 using NUnit.Framework;
+using ServiceStack.Common;
 using ServiceStack.Logging;
 using ServiceStack.Logging.Support.Logging;
 using ServiceStack.Messaging;
@@ -10,83 +11,227 @@ using ServiceStack.Text;
 
 namespace ServiceStack.Redis.Tests
 {
-	[TestFixture]
-	public class BackgroundThreadMessageServiceTests
-	{
-		public class Reverse
-		{
-			public string Value { get; set; }
-		}
+    [TestFixture]
+    public class BackgroundThreadMessageServiceTests
+    {
+        public class Reverse
+        {
+            public string Value { get; set; }
+        }
 
-		public class Rot13
-		{
-			public string Value { get; set; }
-		}
+        public class Rot13
+        {
+            public string Value { get; set; }
+        }
 
-		[TestFixtureSetUp]
-		public void TestFixtureSetUp()
-		{
-			LogManager.LogFactory = new ConsoleLogFactory();
-		}
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            LogManager.LogFactory = new ConsoleLogFactory();
+        }
 
-		private static BackgroundThreadMessageService CreateMqHost()
-		{
-			var redisFactory = new BasicRedisClientManager();
-			var mqHost = new BackgroundThreadMessageService(redisFactory, 2, null);
-			return mqHost;
-		}
+        private static BackgroundThreadMqHost CreateMqHost()
+        {
+            return CreateMqHost(2);
+        }
 
-		[Test]
-		public void Can_Fire_Messages()
-		{
-			var mqHost = CreateMqHost();
-			var mqClient = mqHost.CreateMessageQueueClient();
-			mqClient.Publish(new Reverse { Value = "Demis" });
-			mqClient.Publish(new Reverse { Value = "Bellot" });
-			Thread.Sleep(1000);
-			mqClient.Publish(new Reverse { Value = "ServiceStack" });
-			mqClient.Publish(new Reverse { Value = "Redis" });			
-		}
+        private static BackgroundThreadMqHost CreateMqHost(int noOfRetries)
+        {
+            var redisFactory = new BasicRedisClientManager();
+            try
+            {
+                redisFactory.Exec(redis => redis.FlushAll());
+            }
+            catch (RedisException rex)
+            {
+                Console.WriteLine("WARNING: Redis not started? \n" + rex.Message);
+            }
+            var mqHost = new BackgroundThreadMqHost(redisFactory, noOfRetries, null);
+            return mqHost;
+        }
 
-		[Test]
-		public void Message_does_fire_PubSub()
-		{
-			var redisFactory = new BasicRedisClientManager();
-			var subscription = new RedisSubscription((IRedisNativeClient) redisFactory.GetClient());
+        private static void Publish_4_messages(IMessageQueueClient mqClient)
+        {
+            mqClient.Publish(new Reverse { Value = "Hello" });
+            mqClient.Publish(new Reverse { Value = "World" });
+            mqClient.Publish(new Reverse { Value = "ServiceStack" });
+            mqClient.Publish(new Reverse { Value = "Redis" });
+        }
 
-			subscription.OnMessage = (ch, msg) => Console.WriteLine("{0} from {1}".Fmt(msg, ch));
+        [Test]
+        public void Does_process_messages_sent_before_it_was_started()
+        {
+            var reverseCalled = 0;
 
-			Action a = Can_Fire_Messages;
-			a.BeginInvoke(null, null);
-			subscription.SubscribeToChannels(QueueNames<Reverse>.In);
+            var mqHost = CreateMqHost();
+            mqHost.RegisterHandler<Reverse>(x => { reverseCalled++; return x.Body.Value.Reverse(); });
 
-			//Thread.Sleep(30000);
-		}
+            var mqClient = mqHost.CreateMessageQueueClient();
+            Publish_4_messages(mqClient);
 
-		[Test]
-		public void Can_Start_BackgroundThreadMessageService()
-		{
-			var mqHost = CreateMqHost();
+            mqHost.Start();
+            Thread.Sleep(3000);
 
-			mqHost.RegisterHandler<Reverse>(x => x.Body.Value.Reverse());
-			mqHost.RegisterHandler<Rot13>(x => x.Body.Value.ToRot13());
+            Assert.That(mqHost.GetStats().TotalMessagesProcessed, Is.EqualTo(4));
+            Assert.That(reverseCalled, Is.EqualTo(4));
 
-			mqHost.Start();
-			Thread.Sleep(TimeSpan.FromSeconds(1));
+            mqHost.Dispose();
+        }
 
-			var mqClient = mqHost.CreateMessageQueueClient();
-			mqClient.Publish(new Reverse { Value = "Demis" });
-			mqClient.Publish(new Rot13 { Value = "Bellot" });
+        [Test]
+        public void Does_process_all_messages_and_Starts_Stops_correctly_with_multiple_threads_racing()
+        {
+            var mqHost = CreateMqHost();
 
-			Console.WriteLine(mqHost.GetStats());
+            var reverseCalled = 0;
+            var rot13Called = 0;
 
-			for (var i=0; i > 10; i++) mqHost.Start();
-			mqHost.Stop();
-			for (var i=0; i > 10; i++) mqHost.Start();
+            mqHost.RegisterHandler<Reverse>(x => { reverseCalled++; return x.Body.Value.Reverse(); });
+            mqHost.RegisterHandler<Rot13>(x => { rot13Called++; return x.Body.Value.ToRot13(); });
 
-			Console.WriteLine(mqHost.GetStats());
+            var mqClient = mqHost.CreateMessageQueueClient();
+            mqClient.Publish(new Reverse { Value = "Hello" });
+            mqClient.Publish(new Reverse { Value = "World" });
+            mqClient.Publish(new Rot13 { Value = "ServiceStack" });
 
-			Thread.Sleep(TimeSpan.FromSeconds(5));
-		}
-	}
+            mqHost.Start();
+            Thread.Sleep(3000);
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Started"));
+            Assert.That(mqHost.GetStats().TotalMessagesProcessed, Is.EqualTo(3));
+
+            mqClient.Publish(new Reverse { Value = "Foo" });
+            mqClient.Publish(new Rot13 { Value = "Bar" });
+
+            10.Times(x => ThreadPool.QueueUserWorkItem(y => mqHost.Start()));
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Started"));
+
+            5.Times(x => ThreadPool.QueueUserWorkItem(y => mqHost.Stop()));
+            Thread.Sleep(1000);
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Stopped"));
+
+            10.Times(x => ThreadPool.QueueUserWorkItem(y => mqHost.Start()));
+            Thread.Sleep(3000);
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Started"));
+
+            Console.WriteLine("\n" + mqHost.GetStats());
+
+            Assert.That(mqHost.GetStats().TotalMessagesProcessed, Is.EqualTo(5));
+            Assert.That(reverseCalled, Is.EqualTo(3));
+            Assert.That(rot13Called, Is.EqualTo(2));
+
+            mqHost.Dispose();
+        }
+
+        [Test]
+        public void Only_allows_1_BgThread_to_run_at_a_time()
+        {
+            var mqHost = CreateMqHost();
+
+            mqHost.RegisterHandler<Reverse>(x => x.Body.Value.Reverse());
+            mqHost.RegisterHandler<Rot13>(x => x.Body.Value.ToRot13());
+
+            5.Times(x => ThreadPool.QueueUserWorkItem(y => mqHost.Start()));
+            Thread.Sleep(1000);
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Started"));
+            Assert.That(mqHost.BgThreadCount, Is.EqualTo(1));
+
+            10.Times(x => ThreadPool.QueueUserWorkItem(y => mqHost.Stop()));
+            Thread.Sleep(1000);
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Stopped"));
+
+            ThreadPool.QueueUserWorkItem(y => mqHost.Start());
+            Thread.Sleep(1000);
+            Assert.That(mqHost.GetStatus(), Is.EqualTo("Started"));
+
+            Assert.That(mqHost.BgThreadCount, Is.EqualTo(2));
+
+            Console.WriteLine(mqHost.GetStats());
+
+            mqHost.Dispose();
+        }
+
+        [Test]
+        public void Cannot_Start_a_Disposed_MqHost()
+        {
+            var mqHost = CreateMqHost();
+
+            mqHost.RegisterHandler<Reverse>(x => x.Body.Value.Reverse());
+            mqHost.Dispose();
+
+            try
+            {
+                mqHost.Start();
+                Assert.Fail("Should throw ObjectDisposedException");
+            }
+            catch (ObjectDisposedException) { }
+        }
+
+        [Test]
+        public void Cannot_Stop_a_Disposed_MqHost()
+        {
+            var mqHost = CreateMqHost();
+
+            mqHost.RegisterHandler<Reverse>(x => x.Body.Value.Reverse());
+            mqHost.Start();
+            Thread.Sleep(1000);
+
+            mqHost.Dispose();
+
+            try
+            {
+                mqHost.Stop();
+                Assert.Fail("Should throw ObjectDisposedException");
+            }
+            catch (ObjectDisposedException) { }
+        }
+        
+        public class AlwaysThrows
+        {
+            public string Value { get; set; }
+        }
+
+        [Test]
+        public void Does_retry_messages_with_errors_by_RetryCount()
+        {
+            var retryCount = 3; 
+            var totalRetries = 1 + retryCount; //in total, inc. first try
+
+            var mqHost = CreateMqHost(retryCount);
+
+            var reverseCalled = 0;
+            var rot13Called = 0;
+            
+            mqHost.RegisterHandler<Reverse>(x => { reverseCalled++; return x.Body.Value.Reverse(); });
+            mqHost.RegisterHandler<Rot13>(x => { rot13Called++; return x.Body.Value.ToRot13(); });
+            mqHost.RegisterHandler<AlwaysThrows>(x => { throw new Exception("Always Throwing! " + x.Body.Value); });
+            mqHost.Start();
+
+            var mqClient = mqHost.CreateMessageQueueClient();
+            mqClient.Publish(new AlwaysThrows { Value = "1st" });
+            mqClient.Publish(new Reverse { Value = "Hello" });
+            mqClient.Publish(new Reverse { Value = "World" });
+            mqClient.Publish(new Rot13 { Value = "ServiceStack" });
+
+            Thread.Sleep(3000);
+            Assert.That(mqHost.GetStats().TotalMessagesFailed, Is.EqualTo(1 * totalRetries));
+            Assert.That(mqHost.GetStats().TotalMessagesProcessed, Is.EqualTo(2 + 1));
+
+            5.Times(x => mqClient.Publish(new AlwaysThrows { Value = "#" + x }));
+
+            mqClient.Publish(new Reverse { Value = "Hello" });
+            mqClient.Publish(new Reverse { Value = "World" });
+            mqClient.Publish(new Rot13 { Value = "ServiceStack" });
+            
+            Thread.Sleep(5000);
+
+            Console.WriteLine(mqHost.GetStatsDescription());
+
+            Assert.That(mqHost.GetStats().TotalMessagesFailed, Is.EqualTo((1 + 5) * totalRetries));
+            Assert.That(mqHost.GetStats().TotalMessagesProcessed, Is.EqualTo(6));
+
+            Assert.That(reverseCalled, Is.EqualTo(2 + 2));
+            Assert.That(rot13Called, Is.EqualTo(1 + 1));
+        }
+
+    }
 }
