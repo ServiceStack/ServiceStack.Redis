b869e5ea89581607eb39e77e2b6c70a3fa0a52d1
Sergey Bogdanov <sergey.bogdanov@gmail.com>
2012-09-19 22:40:16 +0300

    - added NuGetPack target for build script

--- build.cmd
+++ build.cmd
@@ -5,4 +5,11 @@ if "%target%" == "" (
    set target=UnitTestsWithoutIntegration
 )
 
+if "%target%" == "NuGetPack" (
+	if "%BUILD_NUMBER%" == "" (
+	 	echo BUILD_NUMBER environment variable is not set.
+		exit;
+	)
+)
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /target:%target% /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
