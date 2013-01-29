24605dda97811d20f18e1a38935b5005e724296a
Demis Bellot <demis.bellot@gmail.com>
2013-01-29 02:14:22 -0500

    INCR to v3.9.35

--- src/ServiceStack.Redis/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Redis/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ServiceStack.Redis")]
-[assembly: AssemblyCopyright("Copyright © ServiceStack 2012")]
+[assembly: AssemblyCopyright("Copyright © ServiceStack 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.32.0")]
+[assembly: AssemblyVersion("3.9.35.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("ServiceStack.Redis.Tests")]
