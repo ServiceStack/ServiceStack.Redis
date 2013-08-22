a41e2f4d8c79e5c141833fe1be060c16d4db2407
mythz <demis.bellot@gmail.com>
2013-08-22 17:10:54 -0400

    Rename to DisablePriorityQueues for better consistency

--- src/ServiceStack.Redis/Messaging/RedisMqServer.cs
+++ src/ServiceStack.Redis/Messaging/RedisMqServer.cs
@@ -66,7 +66,7 @@ namespace ServiceStack.Redis.Messaging
         /// <summary>
         /// Don't listen on any Priority Queues
         /// </summary>
-        public bool DisableAllPriorityQueues
+        public bool DisablePriorityQueues
         {
             set
             {
