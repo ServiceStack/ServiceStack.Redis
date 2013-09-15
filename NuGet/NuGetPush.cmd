940b43339dfee4f0e47c52ef4ce9d48f4fea2bc1
Demis Bellot <demis.bellot@gmail.com>
2013-09-15 16:32:48 -0400

    Keep consistent NuGet structure, update NuGet.exe

---
+++ NuGet/NuGetPush.cmd
@@ -0,0 +1,3 @@
+SET NUGET=..\src\.nuget\nuget
+%NUGET% push ServiceStack.Redis.3.9.60.nupkg
+%NUGET% push ServiceStack.Redis.3.9.60.symbols.nupkg
--- src/ServiceStack.Redis/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Redis/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.9.60.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("3.9.60.0")]
 
 [assembly: InternalsVisibleTo("ServiceStack.Redis.Tests")]
