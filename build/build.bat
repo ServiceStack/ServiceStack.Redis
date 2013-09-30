49b61f487cad3893574035a0fa07933a5dd4f312
Demis Bellot <demis.bellot@gmail.com>
2013-09-30 00:31:38 -0400

    Update build.proj/nuspecs to v4

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=60
+%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=1
