e3e485b89ab8711ed56e235c8e249c470f6fd691
Demis Bellot <demis.bellot@gmail.com>
2014-08-29 16:22:34 -0400

    Update SS dlls

--- build/copy.bat
+++ build/copy.bat
@@ -4,7 +4,4 @@ SET BUILD=Release
 COPY ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* ..\NuGet\lib\net40
 
 COPY ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* ..\..\ServiceStack.Contrib\lib
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* ..\..\ServiceStack.RedisWebServices\lib
-
 COPY ..\src\ServiceStack.Redis\bin\Signed\ServiceStack.Redis.* ..\..\ServiceStack\lib\signed
