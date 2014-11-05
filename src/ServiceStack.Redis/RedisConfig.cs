605c5109510a60b92c2648e6649ce94f27ca3a62
Demis Bellot <demis.bellot@gmail.com>
2014-11-05 17:00:31 -0500

    Add link to CertValidation example

--- src/ServiceStack.Redis/RedisConfig.cs
+++ src/ServiceStack.Redis/RedisConfig.cs
@@ -5,6 +5,8 @@ namespace ServiceStack.Redis
     public class RedisConfig
     {
         public static LocalCertificateSelectionCallback CertificateSelectionCallback { get; set; }
+
+        //Example at: http://msdn.microsoft.com/en-us/library/office/dd633677(v=exchg.80).aspx 
         public static RemoteCertificateValidationCallback CertificateValidationCallback { get; set; }
     }
 }
