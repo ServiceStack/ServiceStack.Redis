33a310828574cdd45bc451d832fe42c7ad9c0963
Andy Berryman <topdev1@gmail.com>
2015-03-13 09:06:55 -0400

    Revert "Update IRedisSentinel.cs"
    This reverts commit a5ce170d3b348749ab998d5af8265062634ba2d7.

--- src/ServiceStack.Redis/IRedisSentinel.cs
+++ src/ServiceStack.Redis/IRedisSentinel.cs
@@ -1,8 +1,13 @@
-using System;
+﻿using System;
 namespace ServiceStack.Redis
 {
     public interface IRedisSentinel : IDisposable
     {
+	// Obsolete
         IRedisClientsManager Setup();
+	IRedisClientsManager Setup(int initialDb, int? poolSizeMultiplier, int? poolTimeOutSeconds);
+
+        IRedisClientsManager Start();
+	IRedisClientsManager Start(int initialDb, int? poolSizeMultiplier, int? poolTimeOutSeconds);
     }
 }
