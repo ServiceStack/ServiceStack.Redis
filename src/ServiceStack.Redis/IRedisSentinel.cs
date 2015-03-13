a5ce170d3b348749ab998d5af8265062634ba2d7
Andy Berryman <topdev1@gmail.com>
2015-03-13 09:04:46 -0400

    Update IRedisSentinel.cs

--- src/ServiceStack.Redis/IRedisSentinel.cs
+++ src/ServiceStack.Redis/IRedisSentinel.cs
@@ -1,13 +1,8 @@
-﻿using System;
+using System;
 namespace ServiceStack.Redis
 {
     public interface IRedisSentinel : IDisposable
     {
-	// Obsolete
         IRedisClientsManager Setup();
-	IRedisClientsManager Setup(int initialDb, int? poolSizeMultiplier, int? poolTimeOutSeconds);
-
-        IRedisClientsManager Start();
-	IRedisClientsManager Start(int initialDb, int? poolSizeMultiplier, int? poolTimeOutSeconds);
     }
 }
