cd42a9dadb384f5dea5007571f8a3e18126afef6
Demis Bellot <demis.bellot@gmail.com>
2015-07-13 12:17:46 +0800

    Update sentinel.conf

--- src/sentinel/reset.cmd
+++ src/sentinel/reset.cmd
@@ -5,3 +5,10 @@ copy /v/y orig\redis-6382\* redis-6382
 del /F /Q redis-6380\dump.rdb
 del /F /Q redis-6381\dump.rdb
 del /F /Q redis-6382\dump.rdb
+
+redis\redis-cli -p 6380 SHUTDOWN NOSAVE
+redis\redis-cli -p 6381 SHUTDOWN NOSAVE
+redis\redis-cli -p 6382 SHUTDOWN NOSAVE
+redis\redis-cli -p 26380 SHUTDOWN NOSAVE
+redis\redis-cli -p 26381 SHUTDOWN NOSAVE
+redis\redis-cli -p 26382 SHUTDOWN NOSAVE
