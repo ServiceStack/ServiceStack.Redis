c4a4f156773afb817ca0d5867d474320fc53ec08
Demis Bellot <demis.bellot@gmail.com>
2015-08-14 16:14:28 +0800

    Add more tests, testing retry behavior

--- tests/ServiceStack.Redis.Tests/RetryCommandTests.cs
+++ tests/ServiceStack.Redis.Tests/RetryCommandTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.Sockets;
 using System.Threading;
 using System.Timers;
@@ -15,6 +16,8 @@ namespace ServiceStack.Redis.Tests
         public void Does_retry_failed_commands()
         {
             //LogManager.LogFactory = new ConsoleLogFactory(debugEnabled: true);
+
+            RedisStats.Reset();
 
             var redisCtrl = new RedisClient(RedisConfig.DefaultHost);
             redisCtrl.FlushAll();
@@ -36,6 +39,106 @@ namespace ServiceStack.Redis.Tests
 
             Assert.That(redis.IncrementValue("retryCounter"), Is.EqualTo(2));
             Assert.That(redis.Get<int>("retryCounter"), Is.EqualTo(2));
+
+            Assert.That(RedisStats.TotalRetryCount, Is.EqualTo(1));
+            Assert.That(RedisStats.TotalRetrySuccess, Is.EqualTo(1));
+            Assert.That(RedisStats.TotalRetryTimedout, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Does_retry_failed_commands_with_SocketException()
+        {
+            RedisStats.Reset();
+
+            var redis = new RedisClient(RedisConfig.DefaultHost);
+            redis.FlushAll();
+
+            Assert.That(redis.IncrementValue("retryCounter"), Is.EqualTo(1));
+
+            redis.OnBeforeFlush = () =>
+            {
+                redis.OnBeforeFlush = null;
+                throw new SocketException();
+            };
+
+            Assert.That(redis.IncrementValue("retryCounter"), Is.EqualTo(2));
+            Assert.That(redis.Get<int>("retryCounter"), Is.EqualTo(2));
+
+            Assert.That(RedisStats.TotalRetryCount, Is.EqualTo(1));
+            Assert.That(RedisStats.TotalRetrySuccess, Is.EqualTo(1));
+            Assert.That(RedisStats.TotalRetryTimedout, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Does_Timeout_with_repeated_SocketException()
+        {
+            RedisConfig.Reset();
+            RedisConfig.DefaultRetryTimeout = 100;
+
+            var redis = new RedisClient(RedisConfig.DefaultHost);
+            redis.FlushAll();
+
+            Assert.That(redis.IncrementValue("retryCounter"), Is.EqualTo(1));
+
+            redis.OnBeforeFlush = () =>
+            {
+                throw new SocketException();
+            };
+
+            try
+            {
+                redis.IncrementValue("retryCounter");
+                Assert.Fail("Should throw");
+            }
+            catch (RedisException ex)
+            {
+                Assert.That(ex.Message, Is.StringStarting("Exceeded timeout"));
+
+                redis.OnBeforeFlush = null;
+                Assert.That(redis.Get<int>("retryCounter"), Is.EqualTo(1));
+
+                Assert.That(RedisStats.TotalRetryCount, Is.GreaterThan(1));
+                Assert.That(RedisStats.TotalRetrySuccess, Is.EqualTo(0));
+                Assert.That(RedisStats.TotalRetryTimedout, Is.EqualTo(1));
+            }
+
+            RedisConfig.Reset();
+        }
+
+        [Test]
+        public void Does_not_retry_when_RetryTimeout_is_Zero()
+        {
+            RedisConfig.Reset();
+            RedisConfig.DefaultRetryTimeout = 0;
+
+            var redis = new RedisClient(RedisConfig.DefaultHost);
+            redis.FlushAll();
+
+            Assert.That(redis.IncrementValue("retryCounter"), Is.EqualTo(1));
+
+            redis.OnBeforeFlush = () =>
+            {
+                throw new SocketException();
+            };
+
+            try
+            {
+                redis.IncrementValue("retryCounter");
+                Assert.Fail("Should throw");
+            }
+            catch (Exception ex)
+            {
+                Assert.That(ex.Message, Is.StringStarting("Exceeded timeout"));
+
+                redis.OnBeforeFlush = null;
+                Assert.That(redis.Get<int>("retryCounter"), Is.EqualTo(1));
+
+                Assert.That(RedisStats.TotalRetryCount, Is.EqualTo(0));
+                Assert.That(RedisStats.TotalRetrySuccess, Is.EqualTo(0));
+                Assert.That(RedisStats.TotalRetryTimedout, Is.EqualTo(1));
+            }
+
+            RedisConfig.Reset();
         }
     }
 }
