f63f8cca9b5fed86afe3c061942e0905ea31df4b
Demis Bellot <demis.bellot@gmail.com>
2016-04-06 16:52:52 -0400

    Explicitly ignore temporal test

--- tests/ServiceStack.Redis.Tests/PooledRedisClientManagerTests.cs
+++ tests/ServiceStack.Redis.Tests/PooledRedisClientManagerTests.cs
@@ -395,7 +395,7 @@ namespace ServiceStack.Redis.Tests
             }
         }
 
-        [Ignore("tempromental integration test")]
+        [Explicit,Ignore("tempromental integration test")]
         [Test]
         public void Can_support_64_threads_using_the_client_simultaneously()
         {
