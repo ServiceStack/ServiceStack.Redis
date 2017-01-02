76554a50a45c687518cba417ed03822b9923dc15
Demis Bellot <demis.bellot@gmail.com>
2017-01-01 21:27:26 -0500

    Update ScanResult.cs

--- src/ServiceStack.Redis/ScanResult.cs
+++ src/ServiceStack.Redis/ScanResult.cs
@@ -16,7 +16,9 @@ namespace ServiceStack.Redis
             for (var i = 0; i < result.Results.Count; i += 2)
             {
                 var key = result.Results[i];
-                var score = result.Results[i + 1].ToDouble();
+                var score = double.Parse(result.Results[i + 1].FromUtf8Bytes(),
+                    NumberStyles.Float,
+                    CultureInfo.InvariantCulture);
                 to[key.FromUtf8Bytes()] = score;
             }
             return to;
