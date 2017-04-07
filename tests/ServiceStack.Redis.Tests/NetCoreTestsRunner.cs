c1a820fc247263468b5430bfa0b021ecf3d69d68
Demis Bellot <demis.bellot@gmail.com>
2017-04-07 05:50:13 -0400

    uncomment NetCore Test Runner

--- tests/ServiceStack.Redis.Tests/NetCoreTestsRunner.cs
+++ tests/ServiceStack.Redis.Tests/NetCoreTestsRunner.cs
@@ -1,3 +1,5 @@
+//NUnitLite isn't recognized in VS2017 - shouldn't need NUnitLite with NUnit 3.5+ https://github.com/nunit/dotnet-test-nunit
+#if false
 using NUnitLite;
 using NUnit.Common;
 using System.Reflection;
@@ -34,3 +36,4 @@ namespace NUnitLite.Tests
         }
     }
 }
+#endif
