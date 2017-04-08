06417170ca6197d525227dd7783ef4c45f4ce2f8
Demis Bellot <demis.bellot@gmail.com>
2017-04-08 03:23:14 -0400

    update copy.bat

--- build/copy.bat
+++ build/copy.bat
@@ -1,7 +1,6 @@
 REM SET BUILD=Debug
 SET BUILD=Release
 
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* ..\NuGet\lib\net45
-
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.Redis\bin\Signed\ServiceStack.Redis.* ..\..\ServiceStack\lib\signed
+COPY ..\src\ServiceStack.Redis\bin\%BUILD%\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.Redis\bin\%BUILD%\netstandard1.3\ServiceStack.Redis.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.Redis\bin\Signed\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib\signed
