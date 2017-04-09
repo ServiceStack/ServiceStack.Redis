4d39462b2f9c61e76f1a0ea0d9fc5577f8787b76
Demis Bellot <demis.bellot@gmail.com>
2017-04-09 03:53:36 -0400

    Add Common.Signed

--- lib/copy.bat
+++ lib/copy.bat
@@ -15,13 +15,12 @@ COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\Signed\net45\ServiceStack.Cl
 
 COPY ..\..\ServiceStack\src\ServiceStack.Common\bin\%BUILD%\net45\ServiceStack.Common.* net45
 COPY ..\..\ServiceStack\src\ServiceStack.Common\bin\%BUILD%\netstandard1.3\ServiceStack.Common.* netstandard1.3
+COPY ..\..\ServiceStack\src\ServiceStack.Common\bin\Signed\net45\ServiceStack.Common.* signed
 
 COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\net45\ServiceStack.dll net45
 COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\net45\ServiceStack.xml net45
 COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\netstandard1.6\ServiceStack.dll netstandard1.6
 COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\netstandard1.6\ServiceStack.xml netstandard1.6
-COPY ..\..\ServiceStack\src\ServiceStack\bin\Signed\net45\ServiceStack.dll signed
-COPY ..\..\ServiceStack\src\ServiceStack\bin\Signed\net45\ServiceStack.xml signed
 
 COPY ..\..\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\net45\ServiceStack.Server.* net45
 COPY ..\..\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\netstandard1.6\ServiceStack.Server.* netstandard1.6
