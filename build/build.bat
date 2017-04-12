087534cb855c9baad0919a935de7eda4ad2b76e2
Demis Bellot <demis.bellot@gmail.com>
2017-04-12 05:18:36 -0400

    Extract .NET Core build into separate build-core.proj

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,5 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
+%MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
+%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
 REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
