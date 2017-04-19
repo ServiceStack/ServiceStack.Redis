73babc087b5502a89c8ce632a3e2a0d37b938cf2
Demis Bellot <demis.bellot@gmail.com>
2017-04-19 10:03:39 -0400

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -2,4 +2,4 @@ SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBui
 
 %MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
 %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
-REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
+REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;PatchVersion=9
