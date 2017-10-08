1ad87ea401a5ed75f0b0f757c46adc0a3039ae3e
Demis Bellot <demis.bellot@gmail.com>
2017-10-08 07:14:47 -0400

    Upgrade to .NET Standard 2.0

--- build/copy.bat
+++ build/copy.bat
@@ -1,6 +1,6 @@
 REM SET BUILD=Debug
 SET BUILD=Release
 
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\netstandard1.3\ServiceStack.Redis.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.Redis\bin\%BUILD%\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib\net45
+COPY ..\src\ServiceStack.Redis\bin\%BUILD%\netstandard2.0\ServiceStack.Redis.* ..\..\ServiceStack\lib\netstandard2.0
 COPY ..\src\ServiceStack.Redis\bin\Signed\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib\signed
