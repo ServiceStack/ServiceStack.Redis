3e52f7edf763608e7efbcb6fc772dd161d802c4a
Demis Bellot <demis.bellot@gmail.com>
2017-10-10 03:42:16 -0400

    Handle Save already in progress error

--- tests/ServiceStack.Redis.Tests/RedisClientTests.cs
+++ tests/ServiceStack.Redis.Tests/RedisClientTests.cs
@@ -247,7 +247,20 @@ namespace ServiceStack.Redis.Tests
         [Test]
         public void Can_Save()
         {
-            Redis.Save();
+            try
+            {
+                Redis.Save();
+            }
+            catch (RedisResponseException e)
+            {
+                // if exception has that message then it still proves that BgSave works as expected.
+                if (e.Message.StartsWith("Can't BGSAVE while AOF log rewriting is in progress")
+                    || e.Message.StartsWith("An AOF log rewriting in progress: can't BGSAVE right now")
+                    || e.Message.StartsWith("Background save already in progress"))
+                    return;
+
+                throw;
+            }
         }
 
         [Test]
@@ -261,7 +274,8 @@ namespace ServiceStack.Redis.Tests
             {
                 // if exception has that message then it still proves that BgSave works as expected.
                 if (e.Message.StartsWith("Can't BGSAVE while AOF log rewriting is in progress")
-                    || e.Message.StartsWith("An AOF log rewriting in progress: can't BGSAVE right now"))
+                    || e.Message.StartsWith("An AOF log rewriting in progress: can't BGSAVE right now")
+                    || e.Message.StartsWith("Background save already in progress"))
                     return;
 
                 throw;
