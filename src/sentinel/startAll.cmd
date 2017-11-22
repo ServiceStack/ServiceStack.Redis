f9a6d85f9090c0e528132bc1e6d36abe41aaa8b7
Demis Bellot <demis.bellot@gmail.com>
2017-11-22 05:23:13 -0500

    Add OSX config

--- src/sentinel/startAll.cmd
+++ src/sentinel/startAll.cmd
@@ -2,9 +2,9 @@ start cmd.exe /e:on /k "redis\redis-server redis-6380\redis.windows.conf"
 start cmd.exe /e:on /k "redis\redis-server redis-6381\redis.windows.conf" 
 start cmd.exe /e:on /k "redis\redis-server redis-6382\redis.windows.conf" 
 
-start cmd.exe /e:on /k "redis\redis-server redis-6380\sentinel.conf --sentinel" 
-start cmd.exe /e:on /k "redis\redis-server redis-6381\sentinel.conf --sentinel" 
-start cmd.exe /e:on /k "redis\redis-server redis-6382\sentinel.conf --sentinel" 
+start cmd.exe /e:on /k "redis\redis-server redis-6380\sentinel.windows.conf --sentinel" 
+start cmd.exe /e:on /k "redis\redis-server redis-6381\sentinel.windows.conf --sentinel" 
+start cmd.exe /e:on /k "redis\redis-server redis-6382\sentinel.windows.conf --sentinel" 
 
 pause
 
