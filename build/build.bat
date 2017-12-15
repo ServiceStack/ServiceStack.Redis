f36de3402019ad058abc7951e599312daa933a10
Demis Bellot <demis.bellot@gmail.com>
2017-12-14 23:51:20 -0500

    Call UpdateVersion before build

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,5 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-REM %MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
-REM %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
-REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;PatchVersion=9
+%MSBUILD% build.proj /target:Default;NuGetPack /property:Configuration=Release;MinorVersion=1;PatchVersion=0
 
 msbuild /p:Configuration=Release ..\src\ServiceStack.Redis.sln
