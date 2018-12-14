6c0970f1be2f6d358f96194883fc8798b2fff3ee
Demis Bellot <demis.bellot@gmail.com>
2018-12-15 03:59:16 +0800

    remove unused copy.bat

--- build/copy.bat
+++
@@ -1,6 +0,0 @@
-REM SET BUILD=Debug
-SET BUILD=Release
-
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib\net45
-COPY ..\src\ServiceStack.Redis\bin\%BUILD%\netstandard2.0\ServiceStack.Redis.* ..\..\ServiceStack\lib\netstandard2.0
-COPY ..\src\ServiceStack.Redis\bin\Signed\net45\ServiceStack.Redis.* ..\..\ServiceStack\lib\signed
