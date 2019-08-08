e40df0c2cc2ded1497605f1ef7133b5f446d93c8
Demis Bellot <demis.bellot@gmail.com>
2019-08-08 08:19:13 -0400

    Upgrade to v5.6.1

--- tests/ServiceStack.Redis.Tests/RedisTemplateTests.cs
+++ tests/ServiceStack.Redis.Tests/RedisTemplateTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using ServiceStack.Script;
 using ServiceStack.Templates;
 
 namespace ServiceStack.Redis.Tests
@@ -8,38 +9,38 @@ namespace ServiceStack.Redis.Tests
         [Test]
         public void Does_build_connection_string()
         {
-            var context = new TemplateContext
+            var context = new ScriptContext
             {
-                TemplateFilters = { new TemplateRedisFilters() }
+                ScriptMethods = { new RedisScripts() }
             };
             context.Container.AddSingleton<IRedisClientsManager>(() => new RedisManagerPool());
             context.Init();
 
-            Assert.That(context.EvaluateTemplate("{{ redisToConnectionString: host:7000?db=1 }}"),
+            Assert.That(context.EvaluateScript("{{ redisToConnectionString: host:7000?db=1 }}"),
                 Is.EqualTo("host:7000?db=1"));
 
-            Assert.That(context.EvaluateTemplate("{{ { host: 'host' } | redisToConnectionString }}"),
+            Assert.That(context.EvaluateScript("{{ { host: 'host' } | redisToConnectionString }}"),
                 Is.EqualTo("host:6379?db=0"));
 
-            Assert.That(context.EvaluateTemplate("{{ { port: 7000 } | redisToConnectionString }}"),
+            Assert.That(context.EvaluateScript("{{ { port: 7000 } | redisToConnectionString }}"),
                 Is.EqualTo("localhost:7000?db=0"));
 
-            Assert.That(context.EvaluateTemplate("{{ { db: 1 } | redisToConnectionString }}"),
+            Assert.That(context.EvaluateScript("{{ { db: 1 } | redisToConnectionString }}"),
                 Is.EqualTo("localhost:6379?db=1"));
 
-            Assert.That(context.EvaluateTemplate("{{ { host: 'host', port: 7000, db: 1 } | redisToConnectionString }}"),
+            Assert.That(context.EvaluateScript("{{ { host: 'host', port: 7000, db: 1 } | redisToConnectionString }}"),
                 Is.EqualTo("host:7000?db=1"));
 
-            Assert.That(context.EvaluateTemplate("{{ { host: 'host', port: 7000, db: 1, password:'secret' } | redisToConnectionString | raw }}"),
+            Assert.That(context.EvaluateScript("{{ { host: 'host', port: 7000, db: 1, password:'secret' } | redisToConnectionString | raw }}"),
                 Is.EqualTo("host:7000?db=1&password=secret"));
 
-            Assert.That(context.EvaluateTemplate("{{ redisConnectionString }}"),
+            Assert.That(context.EvaluateScript("{{ redisConnectionString }}"),
                 Is.EqualTo("localhost:6379?db=0"));
 
-            Assert.That(context.EvaluateTemplate("{{ { db: 1 } | redisChangeConnection }}"),
+            Assert.That(context.EvaluateScript("{{ { db: 1 } | redisChangeConnection }}"),
                 Is.EqualTo("localhost:6379?db=1"));
 
-            Assert.That(context.EvaluateTemplate("{{ redisConnectionString }}"),
+            Assert.That(context.EvaluateScript("{{ redisConnectionString }}"),
                 Is.EqualTo("localhost:6379?db=1"));
         }
     }
