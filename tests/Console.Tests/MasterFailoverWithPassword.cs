78376b201e3b6306276b07d26cfbf7c8df4c5311
Demis Bellot <demis.bellot@gmail.com>
2021-04-21 00:22:56 +0800

    Update RedisSentinel Password example

--- tests/Console.Tests/MasterFailoverWithPassword.cs
+++ tests/Console.Tests/MasterFailoverWithPassword.cs
@@ -9,23 +9,25 @@ namespace ConsoleTests
     {
         public void Execute()
         {
+            string AddPassword(string host) => $"password@{host}";
+
             var sentinelHosts = new[] { "127.0.0.1:26380", "127.0.0.1:26381", "127.0.0.1:26382" };
-            var sentinel = new RedisSentinel(sentinelHosts, masterName: "mymaster");
-            sentinel.HostFilter = host => "password@{0}".Fmt(host);
+            var sentinel = new RedisSentinel(sentinelHosts.Map(AddPassword), masterName: "mymaster") {
+                HostFilter = AddPassword,
+                SentinelHostFilter = AddPassword,
+            };
             var manager = sentinel.Start();
 
-            sentinel.OnWorkerError = exception => Console.WriteLine(exception);
+            sentinel.OnWorkerError = Console.WriteLine;
 
             while (true)
             {
                 try
                 {
                     const string RedisKey = "my Name";
-                    using (var client = manager.GetClient())
-                    {
-                        var result = client.Get<string>(RedisKey);
-                        Console.WriteLine("Redis Key: {0} \t Port: {1}", result, client.Port);
-                    }
+                    using var client = manager.GetClient();
+                    var result = client.Get<string>(RedisKey);
+                    Console.WriteLine("Redis Key: {0} \t Port: {1}", result, client.Port);
                 }
                 catch (Exception ex)
                 {
