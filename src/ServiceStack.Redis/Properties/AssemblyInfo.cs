ada48890e70b7530318395d4775e7097db671d92
Demis Bellot <demis.bellot@gmail.com>
2022-01-24 21:56:44 +0800

    Upgrade to v6

--- src/ServiceStack.Redis/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Redis/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("70a33fa7-9f81-418d-bb25-6a4be6648ae4")]
-[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("6.0.0.0")]
